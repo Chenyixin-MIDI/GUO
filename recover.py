3be0b2de203ffedfe3581efc155c08701cdd56ea
Chenyixin-MIDI <abc11009988@163.com>
2024-09-07 15:48:21 +0800

    Add files via upload

---
+++ recover.py
@@ -0,0 +1,5 @@
+import os
+if os.path.exists("C:/Users/Administrator/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/main.exe"):
+    os.remove("C:/Users/Administrator/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/main.exe")
+if os.path.exists("C:/Users/Administrator/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/Guo.exe"):
+    os.remove("C:/Users/Administrator/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/Guo.exe")
